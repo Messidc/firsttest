11657a766f261328f785f5732e9df21882c12103
dengdanchaoren <dengdanchaoren@gmail.com>
2018-10-17 17:03:48 +0800

    add test adddevice

--- WulianwangAPITest/WulianwangAPITest/Program.cs
+++ WulianwangAPITest/WulianwangAPITest/Program.cs
@@ -13,26 +13,27 @@ namespace WulianwangAPITest
         {
             //Console.WriteLine("Hello World!");
 
-            string serviceAddress = "http://open.189iot.cn:8832/wlwManage/api/auth.do";
-            //string body = "{ \"userName\": \"DHBSQ\",\"secret\": \"hbgz.890\"}";
-            string body = "{ \"userName\": \"ttrr\",\"secret\": \"Chen@190\"}";
+            string serviceAddress = "http://www.189iot.cn:8832/wlwManage/api/auth.do";
+            string body = "{ \"userName\": \"DHBSQ\",\"secret\": \"hbgz.890\"}";
+            //string body = "{ \"userName\": \"ttrr\",\"secret\": \"Chen@190\"}";
             string sss=Post(serviceAddress,body);
             JAuth jp = JsonConvert.DeserializeObject<JAuth>(sss);
 
 
-//123
-            string body1 = "{ \"imei\": \"864183031179194\"}";
-            string sss1 = Post1("http://open.189iot.cn:8832/wlwManage/api/getDeviceInfo.do", body1, jp.accessToken);
+            ////123
+            //            string body1 = "{ \"imei\": \"864183031179194\"}";
+            //            string sss1 = Post1("http://open.189iot.cn:8832/wlwManage/api/getDeviceInfo.do", body1, jp.accessToken);
 
 
-
+            string body3 = "[{\"deviceName\":\"测试新增电表\",\"deviceModName\":\"DahuaElectricityMeter\",\"prodCompanyName\":\"上海大华测控设备厂有限公司\",\"deviceInstId\":\"DDSY879 - Q\",\"iccid\":\"111111111111111111\",\"imei\":\"1111111111111\",\"installAddress\":\"湖北武汉\",\"wgsLatitude\":\"0\",\"wgsLongitude\":\"0\"}]";
+            string sss3 = PostAddDevice("http://www.189iot.cn:8832/wlwManage/api/addDevice.do", body3, jp.accessToken);
 
             //调用魔豆的sourceCommand
-            string body2 = "{ \"imei\": \"869029030511454\",\"orderList\": [{ \"orderName\": \"命令下发\",\"orderValue\": {\"command\":\"0212fefefefe6899999999999968010263e9b516\"}}]}";
-            string sss2 = Post2("http://open.189iot.cn:8832/wlwManage/api/orderCommand.do", body2, jp.accessToken);
+            //string body2 = "{ \"imei\": \"869029030511454\",\"orderList\": [{ \"orderName\": \"命令下发\",\"orderValue\": {\"command\":\"0212fefefefe6899999999999968010263e9b516\"}}]}";
+            //string sss2 = Post2("http://www.189iot.cn:8832/wlwManage/api/orderCommand.do", body2, jp.accessToken);
 
-            Console.WriteLine(sss1);
-            Console.WriteLine(sss2);
+            //Console.WriteLine(sss1);
+            Console.WriteLine(sss3);
             Console.ReadKey();
         }
 
@@ -249,7 +250,7 @@ namespace WulianwangAPITest
             //内容类型
             request.ContentType = "application/json";
 
-            request.Headers.Add("app_key", "ttrr");
+            request.Headers.Add("app_key", "DHBSQ");
             request.Headers.Add("Authorization", "GZWL " + para);
 
             //设置参数，并进行URL编码
@@ -303,5 +304,78 @@ namespace WulianwangAPITest
             return postContent;//返回Json数据
         }
 
+
+
+        /// <summary>
+        ///DC2
+        ///新增设备
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="jsonParas"></param>
+        /// <returns></returns>
+        public static string PostAddDevice(string Url, string jsonParas, string para)
+        {
+            string strURL = Url;
+
+            //创建一个HTTP请求 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strURL);
+            //Post请求方式 
+            request.Method = "POST";
+            //内容类型
+            request.ContentType = "application/json";
+
+            request.Headers.Add("app_key", "DHBSQ");
+            request.Headers.Add("Authorization", "GZWL " + para);
+
+            //设置参数，并进行URL编码
+
+            string paraUrlCoded = jsonParas;//System.Web.HttpUtility.UrlEncode(jsonParas);  
+
+            byte[] payload;
+            //将Json字符串转化为字节 
+            payload = System.Text.Encoding.UTF8.GetBytes(paraUrlCoded);
+            //设置请求的ContentLength  
+            request.ContentLength = payload.Length;
+
+
+
+
+            //发送请求，获得请求流
+
+            Stream writer;
+            try
+            {
+                writer = request.GetRequestStream();//获取用于写入请求数据的Stream对象
+            }
+            catch (Exception)
+            {
+                writer = null;
+                Console.Write("连接服务器失败!");
+            }
+            //将请求参数写入流
+            writer.Write(payload, 0, payload.Length);
+            writer.Close();//关闭请求流
+
+            //String strValue = "";//strValue为http响应所返回的字符流
+            HttpWebResponse response;
+            try
+            {
+                //获得响应流
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                response = ex.Response as HttpWebResponse;
+            }
+
+            Stream s = response.GetResponseStream();
+
+            //Stream postData = Request.InputStream;
+            StreamReader sRead = new StreamReader(s);
+            string postContent = sRead.ReadToEnd();
+            sRead.Close();
+
+            return postContent;//返回Json数据
+        }
     }
 }
